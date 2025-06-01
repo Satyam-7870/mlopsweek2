7fa7c352d38ce9c941cdad9ae053239af60451f6
Satyam Thakur <131132295+Satyam-7870@users.noreply.github.com>
2025-06-01 15:19:55 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pandas.plotting import parallel_coordinates
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import metrics
+train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
+X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+y_train = train.species
+X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+y_test = test.species
+import pickle
+import joblib
+
+joblib.dump(mod_dt, "artifacts/model.joblib")
